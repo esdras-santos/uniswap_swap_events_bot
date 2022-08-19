06e8a189ccf8c1bc20a0123dda7289e06bf11033
esdras-santos <esantoz@protonmail.com>
2022-08-19 11:48:03 -0300

    Need Review

---
+++ src/agent.spec.ts
@@ -0,0 +1,20 @@
+import {
+  FindingType,
+  FindingSeverity,
+  Finding,
+  HandleTransaction,
+  createTransactionEvent,
+  ethers,
+} from "forta-agent";
+
+describe("high tether transfer agent", () => {
+  let handleTransaction: HandleTransaction;
+  const mockTxEvent = createTransactionEvent({} as any);
+
+  beforeAll(() => {
+    // handleTransaction = agent.handleTransaction;
+  });
+
+
+  
+});
---
+++ src/agent.ts
@@ -0,0 +1,33 @@
+import {
+  BlockEvent,
+  Finding,
+  HandleBlock,
+  HandleTransaction,
+  TransactionEvent,
+  FindingSeverity,
+  FindingType,
+  LogDescription,
+} from "forta-agent";
+
+import {UNISWAP_FACTORY_ABI, SWAP_LOG} from "./utils";
+
+
+export function provideHandleTransaction(factoryAbi: string, swapLog: string) : HandleTransaction{
+  return async (txEvent: TransactionEvent) : Promise<Finding[]> => {
+    const findings: Finding[] = []
+    // get the swap event
+    const swapLogs: LogDescription[] = txEvent.filterLog(swapLog)
+
+    // verify if the log is emmitted by one of the pools of uniswap 
+    swapLogs.forEach((log)=>{
+
+      log.address
+    })
+
+    return findings
+  }
+}
+
+export default {
+  handleTransaction: provideHandleTransaction(UNISWAP_FACTORY_ABI, SWAP_LOG),
+};
