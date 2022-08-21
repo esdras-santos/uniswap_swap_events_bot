ded50430d646c71ec84d6b1c8520374fb6c7a139
esdras-santos <esantoz@protonmail.com>
2022-08-21 18:02:12 -0300

    Need Review

--- src/agent.ts
+++ src/agent.ts
@@ -1,28 +1,48 @@
 import {
-  BlockEvent,
   Finding,
-  HandleBlock,
   HandleTransaction,
   TransactionEvent,
   FindingSeverity,
   FindingType,
   LogDescription,
-  ethers
 } from "forta-agent";
+import { getPoolFactory } from "./pool";
 
-import {UNISWAP_FACTORY_ABI, SWAP_LOG} from "./utils";
+import {UNISWAP_FACTORY_ADDRESS, SWAP_LOG} from "./utils";
 
 
-export function provideHandleTransaction(factoryAbi: string, swapLog: string) : HandleTransaction{
+export function provideHandleTransaction(factoryAddress: string, swapLog: string) : HandleTransaction{
   return async (txEvent: TransactionEvent) : Promise<Finding[]> => {
     const findings: Finding[] = []
-    // get the swap event
-    const swapLogs: LogDescription[] = txEvent.filterLog(swapLog)
 
-    // verify if the log is emmitted by one of the pools of uniswap 
-    swapLogs.forEach((log)=>{
-      log.args.
-      log.address
+    // catch the swap event
+    const swapLogs: LogDescription[] = txEvent.filterLog(swapLog)
+ 
+    swapLogs.forEach(async (log)=>{
+      console.log(log.address)
+      // cathc the factory address in the pool
+      const factory = await getPoolFactory(log.address)
+
+      // compare with the UniswapV3 factory address and then push to findings
+      if(factory === factoryAddress){
+        const { agentId, metadata, chainIds } = log.args;
+        
+        findings.push(
+          Finding.fromObject({
+            name: "Swap detector",
+            description: "Alerts every time a swap occurs on uniswap",
+            alertId: "UNISWAP-SWAP-EVENT",
+            severity: FindingSeverity.Info,
+            type: FindingType.Info,
+            protocol: "uniswap-v3",
+            metadata: {
+              agentId: agentId.toString(),
+              metadata,
+              chainIds: chainIds.toString(),
+            },
+          })
+        );
+      }
     })
 
     return findings
@@ -30,5 +50,5 @@ export function provideHandleTransaction(factoryAbi: string, swapLog: string) : 
 }
 
 export default {
-  handleTransaction: provideHandleTransaction(UNISWAP_FACTORY_ABI, SWAP_LOG),
+  handleTransaction: provideHandleTransaction(UNISWAP_FACTORY_ADDRESS, SWAP_LOG),
 };
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
 export const SWAP_LOG: string = "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick);"
-export const UNISWAP_FACTORY_ABI: string = "function getPool(address tokenA, address tokenB, uint24 fee) external view returns (address pool);"
+export const UNISWAP_FACTORY_ADDRESS: string = ""
